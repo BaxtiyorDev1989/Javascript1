dde8e46db2a0d9fb6c32ea26b1674475b1edaf40
BaxtiyorDev1989 <baxtiyor.nosirov.2015@gmail.com>
2020-02-06 21:20:13 +0500

    save javascript file

---
+++ script.js
@@ -0,0 +1,20 @@
+ var userName = prompt('Введите своё имя');
+ console.log('Меня зовут ' + userName);
+ var userAge = prompt('Введите свой возраст');
+ console.log('Мне ' + userAge);
+ console.log(Number(userAge)); 
+ var math = prompt('Решите пример 6 + 3 = ?' )
+ console.log('Пример 1: 6 + 3 = 9 Ваш ответ ' + math);
+ var math = prompt('Решите пример 20 - 5 = ?' )
+ console.log('Пример 2: 20 - 5 = 15 Ваш ответ ' + math);
+ var math = prompt('Решите пример 15 * 2 = ?' )
+ console.log('Пример 3: 15 * 2 = 30 Ваш ответ ' + math);
+ var math = prompt('Решите пример 8 / 4 = ?' )
+ console.log('Пример 4: 8 / 4 = 2 Ваш ответ ' + math);
+ var math = prompt('Решите пример 10 % 3 = ?' )
+ console.log('Пример 1: 10 % 3 = 1 Ваш ответ ' + math);
+ alert('Откройте консоль для просмотра')
+ 
+ 
+ 
+ 
